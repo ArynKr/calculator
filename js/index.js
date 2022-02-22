20ded69425a483e417de662d60f8e687e78953b6
Aryan Kumar <aryankumar1504@gmail.com>
2022-02-22 17:45:46 +0530

    upd: keyboard tab, enter bug removed

--- js/index.js
+++ js/index.js
@@ -4,26 +4,55 @@ const equals = document.querySelectorAll('.eq');
 const delbtn = document.querySelector('.clr');
 const clearbtn = document.querySelector('.allclr');
 const nums = document.querySelectorAll('.num');
+const btns = document.querySelectorAll('.btn');
 
 let curr = '0';
 let num1 = 0,
 	num2 = 0;
 let operation = '';
 
+/* Disable Tab functionality */
+document.onkeydown = function (t) {
+	if (t.key == 'Tab') {
+		return false;
+	}
+};
+
+/* Add no focus to all buttons */
+Array.from(btns).forEach((btn) => {
+	btn.addEventListener('mousedown', noFocus);
+});
+
+/* No focus on click */
+function noFocus(e) {
+	e.preventDefault();
+}
+
+/* Reset the calculator */
+function reset() {
+	curr = '0';
+	num1 = 0;
+	num2 = 0;
+	operation = '';
+	updateDisplay(curr);
+}
+
 document.addEventListener('keyup', (e) => {
 	console.log(e.key);
-	if (isFinite(e.key) || e.key == '.') {
+	if (e.key == 'Escape') {
+		reset();
+	} else if (isFinite(e.key) || e.key == '.') {
 		append(e.key);
 		updateDisplay(curr);
-		console.log(num1, num2, curr);
+		// console.log(num1, num2, curr);
 	} else if ('+-*/x'.includes(e.key)) {
 		operation = e.key;
 		num1 = curr === '' ? num1 : parseFloat(curr);
 		curr = '';
 		updateDisplay(curr);
-		console.log(num1, num2, curr, operation);
+		// console.log(num1, num2, curr, operation);
 	} else if (e.key === '=') {
-		console.log('eq');
+		// console.log('eq');
 	} else if (e.key === 'Backspace') {
 		pop();
 		updateDisplay(curr);
@@ -61,11 +90,19 @@ function append(val) {
 	curr += val;
 }
 
+/* Resume the equal button */
+function startInteraction() {
+	Array.from(equals).forEach((equal) => {
+		equal.addEventListener('click', equalHandler);
+	});
+}
+
 /* Integer Clicks */
 Array.from(nums).forEach((num) => {
 	num.addEventListener('click', () => {
+		startInteraction();
 		const val = num.innerText;
-		console.dir(val);
+		// console.dir(val);
 		append(val);
 		updateDisplay(curr);
 	});
@@ -74,6 +111,7 @@ Array.from(nums).forEach((num) => {
 /* Operator Click */
 Array.from(operators).forEach((op) => {
 	op.addEventListener('click', () => {
+		startInteraction();
 		operation = op.innerHTML;
 		num1 = curr === '' ? num1 : parseFloat(curr);
 		curr = '';
@@ -81,17 +119,20 @@ Array.from(operators).forEach((op) => {
 	});
 });
 
+/* Equal Button click handler */
+const equalHandler = () => {
+	if (operation === '') return;
+	num2 = parseFloat(curr);
+	curr = '';
+
+	solve();
+	// console.log(num1, num2, curr);
+	updateDisplay(curr);
+};
+
 /* Equal Click */
 Array.from(equals).forEach((equal) => {
-	equal.addEventListener('click', () => {
-		if (operation === '') return;
-		num2 = parseFloat(curr);
-		curr = '';
-
-		solve();
-		console.log(num1, num2, curr);
-		updateDisplay(curr);
-	});
+	equal.addEventListener('click', equalHandler);
 });
 
 /* Display Update */
@@ -101,6 +142,9 @@ function updateDisplay(str) {
 
 /* Solve Function */
 function solve() {
+	Array.from(equals).forEach((equal) => {
+		equal.removeEventListener('click', equalHandler);
+	});
 	let val = 0;
 	switch (operation) {
 		case '+':
@@ -120,9 +164,16 @@ function solve() {
 		default:
 			break;
 	}
+	if (isNaN(val)) {
+		curr = 'Error🤔';
+		return;
+	}
+	if (val == Infinity) {
+		curr = 'Infinity♾️';
+		return;
+	}
 	val = Math.round(val * 10000) / 10000.0;
 	curr = val.toString();
-	console.log(curr);
 }
 
 /* Backspace click */
